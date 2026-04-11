db7e580a9442591ea4691af92cb9fabdad8fe683
vonproject7912 <vonpr@MyAcer.localdomain>
2026-04-11 16:31:52 +0200

    Minishell starter

---
+++ 42CommonCore/M.3/Minishell/minishell.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vonpr <vonpr@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/11 16:20:16 by vonpr             #+#    #+#             */
+/*   Updated: 2026/04/11 16:26:51 by vonpr            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef enum es_type
+{
+	WORD,
+	PIPE,
+	REDIR_IN,
+	REDIR_OUT,
+	REDIR_APPEND,
+	HEREDOC,
+}					t_type;
+
+typedef struct s_token
+{
+	t_type			type;
+	char			*value;
+	int				quote;
+	struct s_token	*next;
+}					t_token;
+
+#endif

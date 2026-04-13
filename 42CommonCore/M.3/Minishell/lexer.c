38a2e3823713b492bfd7e875cd808e531d1f6954
vonproject7912 <vonpr@MyAcer.localdomain>
2026-04-13 15:07:38 +0200

    MOUHAHAH

--- 42CommonCore/M.3/Minishell/lexer.c
+++ 42CommonCore/M.3/Minishell/lexer.c
@@ -6,19 +6,37 @@
 /*   By: vonpr <vonpr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 16:28:36 by vonpr             #+#    #+#             */
-/*   Updated: 2026/04/11 16:29:14 by vonpr            ###   ########.fr       */
+/*   Updated: 2026/04/13 15:05:10 by vonpr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void lexer(char **str)
+t_token	*lexer(char *str, int *lst_ext)
 {
-    int i;
+	int		i;
+	t_token	*my_tokens;
 
-    i = 0;
-    while(str[i])
-    {
-        
-    }
+	i = 0;
+	my_tokens = NULL;  // init my linked list
+	if (quotes_checking(str)) // check for unclosed quotes
+		return (err(lst_ext, "minishell: syntax error: unclosed quote"), NULL);
+	while (str[i] == ' ' || str[i] == '\t')
+		i++; // skip space
+	if (!str[i])
+	    return (NULL); // early return for empty input
+	while (str[i]) // loop and fill my tokens
+	{
+	    if (str[i] == '\'')
+	        // function for single quotes
+	    else if (str[i] == '"')
+	        // function for double quotes
+	    else if (str[i] == ' ' || str[i] == '\t')
+	        i++;
+	    else if (is_operator(str[i]))
+	        // function handle_operators
+	    else
+	        // function for regular words until space or operator
+	}
+	return (my_tokens);
 }
---
+++ 42CommonCore/M.3/Minishell/lexer_utils.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vonpr <vonpr@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/13 12:13:58 by vonpr             #+#    #+#             */
+/*   Updated: 2026/04/13 15:04:29 by vonpr            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_putstr(int fd, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(fd, &str[i], 1);
+		i++;
+	}
+}
+
+// Checks if all quotes in the string are closed.
+int	quotes_checking(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\'')
+		{
+			i++;
+			while (str[i] && str[i] != '\'')
+				i++;
+			if (!str[i])
+				return (1);
+		}
+		else if (str[i] == '"')
+		{
+			i++;
+			while (str[i] && str[i] != '"')
+				i++;
+			if (!str[i])
+				return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	is_operator(char c)
+{
+	if ((c == '|') || (c == '<') || (c == '>'))
+		return (1);
+	return (0);
+}
+
+void	err(int *lst_ext, char *str)
+{
+	ft_putstr(2, str);
+	*lst_ext = 2;
+}

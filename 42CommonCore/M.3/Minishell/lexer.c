ab57188f18ac059346040214cd61b62dd9cfe9f5
vonproject7912 <vonpr@MyAcer.localdomain>
2026-04-14 11:26:41 +0200

    MOUHAHAH

--- 42CommonCore/M.3/Minishell/lexer.c
+++ 42CommonCore/M.3/Minishell/lexer.c
@@ -6,44 +6,82 @@
 /*   By: vonpr <vonpr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 16:28:36 by vonpr             #+#    #+#             */
-/*   Updated: 2026/04/14 10:21:52 by vonpr            ###   ########.fr       */
+/*   Updated: 2026/04/14 11:26:24 by vonpr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// typedef enum es_type
-// {
-// 	WORD,
-// 	PIPE,
-// 	REDIR_IN,
-// 	REDIR_OUT,
-// 	REDIR_APPEND,
-// 	HEREDOC,
-// }					t_type;
-
-// typedef struct s_token
-// {
-// 	t_type			type;
-// 	char			*value;
-// 	int				quote;
-// 	struct s_token	*next;
-// }					t_token;
-
-void handle_single_quote(int *i, char *str, t_token	**my_tokens)
+void	handle_single_quote(int *i, char *str, t_token **my_tokens)
 {
-	int start;
-	t_token *token;
+	int		start;
+	t_token	*token;
 
 	token = new_token();
 	token->quote = 1;
 	token->type = WORD;
-	(*i)++; // pass the '
+	(*i)++;
 	start = *i;
-	while(str[*i] && str[*i] != '\'')
+	while (str[*i] && str[*i] != '\'')
 		(*i)++;
 	token->value = ft_strndup(str + start, *i - start);
-	(*i)++; // pass the end '
+	(*i)++;
+	add_token(my_tokens, token);
+}
+
+void	handle_double_quote(int *i, char *str, t_token **my_tokens)
+{
+	int		start;
+	t_token	*token;
+
+	token = new_token();
+	token->quote = 2;
+	token->type = WORD;
+	(*i)++;
+	start = *i;
+	while (str[*i] && str[*i] != '"')
+		(*i)++;
+	token->value = ft_strndup(str + start, *i - start);
+	(*i)++;
+	add_token(my_tokens, token);
+}
+
+void	handle_anything_else(int *i, char *str, t_token **my_tokens)
+{
+	int		start;
+	t_token	*token;
+
+	token = new_token();
+	token->quote = 0;
+	token->type = WORD;
+	start = *i;
+	while (str[*i] && str[*i] != ' ' && str[*i] != '\t'
+		&& !is_operator(str[*i]))
+		(*i)++;
+	token->value = ft_strndup(str + start, *i - start);
+	add_token(my_tokens, token);
+}
+
+void	handle_operators(int *i, char *str, t_token **my_tokens)
+{
+	t_token	*token;
+
+	token = new_token();
+	token->quote = 0;
+	if (str[*i] == '|')
+		token->type = PIPE;
+	else if (str[*i] == '>' && str[*i + 1] == '>')
+		token->type = REDIR_APPEND;
+	else if (str[*i] == '<' && str[*i + 1] == '<')
+		token->type = HEREDOC;
+	else if (str[*i] == '<')
+		token->type = REDIR_IN;
+	else if (str[*i] == '>' && str[*i + 1] != '>')
+		token->type = REDIR_OUT;
+	token->value = NULL;
+	(*i)++;
+	if (token->type == REDIR_APPEND || token->type == HEREDOC)
+		(*i)++;
 	add_token(my_tokens, token);
 }
 
@@ -53,40 +91,25 @@ t_token	*lexer(char *str, int *lst_ext)
 	t_token	*my_tokens;
 
 	i = 0;
-	my_tokens = NULL;  // init my linked list
-	if (quotes_checking(str)) // check for unclosed quotes
+	my_tokens = NULL;
+	if (quotes_checking(str))
 		return (err(lst_ext, "minishell: syntax error: unclosed quote"), NULL);
 	while (str[i] == ' ' || str[i] == '\t')
-		i++; // skip space
+		i++;
 	if (!str[i])
-	    return (NULL); // early return for empty input
-	while (str[i]) // loop and fill my tokens
+		return (NULL);
+	while (str[i])
 	{
-	    if (str[i] == '\'')
-	        handle_single_quote(&i, str, &my_tokens);
-	    else if (str[i] == '"')
-	        // function for double quotes
-	    else if (str[i] == ' ' || str[i] == '\t')
-	        i++;
-	    else if (is_operator(str[i]))
-	        // function handle_operators
-	    else
-	        // function for regular words until space or operator
+		if (str[i] == ' ' || str[i] == '\t')
+			i++;
+		else if (str[i] == '\'')
+			handle_single_quote(&i, str, &my_tokens);
+		else if (str[i] == '"')
+			handle_double_quote(&i, str, &my_tokens);
+		else if (is_operator(str[i]))
+			handle_operators(&i, str, &my_tokens);
+		else
+			handle_anything_else(&i, str, &my_tokens);
 	}
 	return (my_tokens);
-}
-
-void handle_double_quote()
-{
-	
-}
-
-void handle_operators()
-{
-	
-}
-
-void handle_anything_else()
-{
-	
 }

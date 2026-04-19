11b262479e641e9337ec34e81584bae16f197160
vonproject7912 <vonpr@MyAcer.localdomain>
2026-04-19 09:46:41 +0200

    parsing utils

---
+++ 42CommonCore/M.3/Minishell/parsing_utils.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vonpr <vonpr@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/19 09:45:59 by vonpr             #+#    #+#             */
+/*   Updated: 2026/04/19 09:46:19 by vonpr            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_cmd	*new_cmd(void)
+{
+	t_cmd	*cmd;
+
+	cmd = malloc(sizeof(t_cmd));
+	if (!cmd)
+		return (NULL);
+	cmd->argv = NULL;
+	cmd->redirs = NULL;
+	cmd->next = NULL;
+	return (cmd);
+}
+
+void	add_cmd(t_cmd **lst, t_cmd *new)
+{
+	t_cmd	*current;
+
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	current = *lst;
+	while (current->next)
+		current = current->next;
+	current->next = new;
+}

818af82c77acbf7b42515dc8a8230fcdc1e26ef4
vonproject7912 <vonpr@MyAcer.localdomain>
2026-04-21 11:14:30 +0200

    update

--- 42CommonCore/M.3/Minishell/minishell.h
+++ 42CommonCore/M.3/Minishell/minishell.h
@@ -6,7 +6,7 @@
 /*   By: vonpr <vonpr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 16:20:16 by vonpr             #+#    #+#             */
-/*   Updated: 2026/04/19 12:26:36 by vonpr            ###   ########.fr       */
+/*   Updated: 2026/04/21 11:12:34 by vonpr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,7 @@ int					ft_strlen(char *str);
 void				ft_putstr(int fd, char *str);
 char				*ft_strndup(char *str, int len);
 char				*ft_strjoin(char *s1, char *s2);
+char				*ft_strdup(char *str);
 
 // lexer utils
 int					quotes_checking(char *str);
@@ -72,14 +73,14 @@ void				merge_last_token(t_token *my_tokens, char *new_str);
 void				token_routine(int *i, int *adj, char *str,
 						t_token **my_tokens);
 
-// lexer
-void				handle_single_quote(int *i, int *adj, char *str,
+// actual lexer
+void				token_routine(int *i, int *adj, char *str,
 						t_token **my_tokens);
 void				handle_double_quote(int *i, int *adj, char *str,
 						t_token **my_tokens);
-void				handle_anything_else(int *i, int *adj, char *str,
-						t_token **my_tokens);
 void				handle_operators(int *i, int *adj, char *str,
+						t_token **my_tokens);
+void				handle_anything_else(int *i, int *adj, char *str,
 						t_token **my_tokens);
 t_token				*lexer(int *lst_ext, char *str);
 

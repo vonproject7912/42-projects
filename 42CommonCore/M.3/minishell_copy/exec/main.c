de0df00f0cb11d123b5fb6051e00ba09938b3376
vonproject7912 <vonpr@MyAcer.localdomain>
2026-04-29 17:00:17 +0200

    VON7912

--- 42CommonCore/M.3/minishell_copy/exec/main.c
+++
@@ -1,8 +0,0 @@
-#include "../minishell.h"
-
-int	main(int argc, char **argv, char **envp)
-{
-	(void)argc;
-	(void)argv;
-	return (shell_loop(envp));
-}
--- 42CommonCore/M.3/minishell_copy/exec/minishell.h
+++
@@ -1,6 +0,0 @@
-#ifndef EXEC_MINISHELL_H
-# define EXEC_MINISHELL_H
-
-# include "../minishell.h"
-
-#endif

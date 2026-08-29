a1b5f13073677bd63ecf13d56e2127215f6c430b
vonproject7912 <vonpr@MyAcer.localdomain>
2026-08-29 05:49:01 +0200

    VON7912

--- 42CommonCore/M.4/Cub3d/init/init.c
+++ 42CommonCore/M.4/Cub3d/init/init.c
@@ -6,7 +6,7 @@
 /*   By: vonpr <vonpr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/26 08:49:35 by vonpr             #+#    #+#             */
-/*   Updated: 2026/08/29 02:53:44 by vonpr            ###   ########.fr       */
+/*   Updated: 2026/08/29 05:48:39 by vonpr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 // mlx connection
 int init_mlx(t_game *game)
 {
+	// init the sizes
+	game->screen.width = WIDTH;
+	game->screen.height = HEIGHT;
 	// init the mlx connection to the x server
 	game->mlx_connection = mlx_init(); 
 	if (game->mlx_connection == NULL) // small check
@@ -22,31 +25,33 @@ int init_mlx(t_game *game)
 	// create the window
 	game->window = mlx_new_window(game->mlx_connection, WIDTH, HEIGHT, "wtf");
 	if (!game->window) // failed window creation
-	{
-		mlx_destroy_display(game->mlx_connection); // unlink 
-		free(game->mlx_connection); // free
 		return (1);
-	}
 	// Screen / image creation
 	game->screen.img_ptr = mlx_new_image(game->mlx_connection, WIDTH, HEIGHT);
 	if (game->screen.img_ptr == NULL)
-	{
-		mlx_destroy_window(game->mlx_connection, game->window);
-		mlx_destroy_display(game->mlx_connection);
-		free(game->mlx_connection);
 		return (1);
-	}
 	// get the screen address
 	game->screen.addr = mlx_get_data_addr(game->mlx_connection, 
 		&game->screen.bits_per_pixel, &game->screen.line_length, &game->screen.endian);
 	return (0);
 }
 
-// init player
-int init_player(t_game *game, t_map *map)
+void free_game(t_game *game)
 {
-	
+	// free the image before the connection
+	if (game->screen.img_ptr)
+		mlx_destroy_image(game->mlx_connection, game->screen.img_ptr);
+	// free the window
+	if (game->window)
+		mlx_destroy_window(game->mlx_connection, game->window);
+	// free the connection itself
+	if (game->mlx_connection)
+	{
+		mlx_destroy_display(game->mlx_connection);
+		free(game->mlx_connection);
+	}
 }
 
+// init player
 // init texture
 // init picture

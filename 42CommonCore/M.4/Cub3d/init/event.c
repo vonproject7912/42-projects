eee898da0cf7ee03ce29b5ffe40a0e5e827b9e9c
vonproject7912 <vonpr@MyAcer.localdomain>
2026-08-29 10:31:34 +0200

    VON7912

--- 42CommonCore/M.4/Cub3d/init/event.c
+++ 42CommonCore/M.4/Cub3d/init/event.c
@@ -6,7 +6,7 @@
 /*   By: vonpr <vonpr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/28 10:19:47 by vonpr             #+#    #+#             */
-/*   Updated: 2026/08/29 04:45:20 by vonpr            ###   ########.fr       */
+/*   Updated: 2026/08/29 10:29:26 by vonpr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,39 +16,50 @@
 void even_handling(t_game *game)
 {
 	mlx_hook(game->window, KeyPress, KeyPressMask, key_handling, game);
+	mlx_hook(game->window, KeyRelease, KeyReleaseMask, key_release_handling, game);
 	mlx_hook(game->window, DestroyNotify, StructureNotifyMask, Close_window, game);
+	mlx_loop_hook(game->mlx_connection, render, game);
 }
 
 int key_handling(int keysym, t_game *game)
 {
 	if (keysym == KEY_W)
-		game->player.pos_y -= 0.7;
+		game->player.move_up = 1;
 	if (keysym == KEY_A)
-		game->player.pos_x -= 0.7;
+		game->player.move_left = 1;
 	if (keysym == KEY_S)
-		game->player.pos_y += 0.7;
+		game->player.move_down = 1;
 	if (keysym == KEY_D)
-		game->player.pos_x += 0.7;
+		game->player.move_right = 1;
 	if (keysym == KEY_LEFT)
-		game->player.dir_x -= 0.4;
+		game->player.rotate_left = 1;
 	if (keysym == KEY_RIGHT)
-		game->player.dir_x += 0.4;
+		game->player.rotate_right = 1;
 	if (keysym == KEY_ESC)
 		Close_window(game);
-	// randering function
+	return (0);
+}
+
+int key_release_handling(int keysym, t_game *game)
+{
+	if (keysym == KEY_W)
+		game->player.move_up = 0;
+	if (keysym == KEY_A)
+		game->player.move_left = 0;
+	if (keysym == KEY_S)
+		game->player.move_down = 0;
+	if (keysym == KEY_D)
+		game->player.move_right = 0;
+	if (keysym == KEY_LEFT)
+		game->player.rotate_left = 0;
+	if (keysym == KEY_RIGHT)
+		game->player.rotate_right = 0;
 	return (0);
 }
 
 int Close_window(t_game *game)
 {
-	int i;
-
-	i = 0;
-	while (i < 4)
-		mlx_destroy_image(game->mlx_connection, game->textures[i++].img.img_ptr);
-	mlx_destroy_image(game->mlx_connection, game->screen.img_ptr);
-	mlx_destroy_window(game->mlx_connection, game->window);
-	mlx_destroy_display(game->mlx_connection);
-	free(game->mlx_connection);
+	free_mlx(game);
 	exit(EXIT_SUCCESS);
+	return (0);
 }

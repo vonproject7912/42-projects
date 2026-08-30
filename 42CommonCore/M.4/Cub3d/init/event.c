5bfa75789bf5aeb58c647bb064a3dbb1f6b6e4c7
vonproject7912 <vonpr@MyAcer.localdomain>
2026-08-30 13:30:20 +0200

    VON7912

--- 42CommonCore/M.4/Cub3d/init/event.c
+++ 42CommonCore/M.4/Cub3d/init/event.c
@@ -6,7 +6,7 @@
 /*   By: vonpr <vonpr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/28 10:19:47 by vonpr             #+#    #+#             */
-/*   Updated: 2026/08/30 13:24:09 by vonpr            ###   ########.fr       */
+/*   Updated: 2026/08/30 13:26:17 by vonpr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	even_handling(t_game *game)
 	mlx_hook(game->window, KeyPress, KeyPressMask, key_handling, game);
 	mlx_hook(game->window, KeyRelease, KeyReleaseMask, key_release_handling,
 		game);
-	mlx_hook(game->window, DestroyNotify, StructureNotifyMask, Close_window,
+	mlx_hook(game->window, DestroyNotify, StructureNotifyMask, close_window,
 		game);
 	mlx_loop_hook(game->mlx_connection, render, game);
 }
@@ -38,7 +38,7 @@ int	key_handling(int keysym, t_game *game)
 	if (keysym == KEY_RIGHT)
 		game->player.rotate_right = 1;
 	if (keysym == KEY_ESC)
-		Close_window(game);
+		close_window(game);
 	return (0);
 }
 
@@ -59,7 +59,7 @@ int	key_release_handling(int keysym, t_game *game)
 	return (0);
 }
 
-int	Close_window(t_game *game)
+int	close_window(t_game *game)
 {
 	free_mlx(game);
 	exit(EXIT_SUCCESS);
